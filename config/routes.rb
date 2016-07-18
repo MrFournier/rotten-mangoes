41ccfc4b19ed4452c89891d1b5d0fc2bdf9a478d
MrFournier <troy_fournier@live.com>
2016-07-18 21:32:33 +0000

    Completed up to part 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # get 'movies/index'
+
+  # get 'movies/show'
+
+  # get 'movies/new'
+
+  # get 'movies/edit'
+
+  resources :movies
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
