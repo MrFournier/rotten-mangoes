ee6d6ea88c5e58dad4e033fabd43444557a684d8
MrFournier <troy_fournier@live.com>
2016-07-19 04:17:24 +0000

    Completed up to section 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'users/new'
+
+  get 'users/create'
+
   # get 'movies/index'
 
   # get 'movies/show'
@@ -8,6 +16,8 @@ Rails.application.routes.draw do
   # get 'movies/edit'
 
   resources :movies
-
+  resources :users, only: [:new, :create
+  resources :sessions, only: [:new, :create]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
